d437c299498fca12e3dca8a17f07f264f17cbc4b
davis <davisandrewstreet@gmail.com>
2022-03-26 23:43:50 -0500

    made project

---
+++ quote_gen/quote_gen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quote_gen
+//
+//  Created by davis street on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
