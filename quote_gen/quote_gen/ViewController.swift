67182359a56aa4a4f002417d3183d04cde33a3b2
davis <davisandrewstreet@gmail.com>
2022-03-27 17:43:45 -0500

    got actually started

--- quote_gen/quote_gen/ViewController.swift
+++ quote_gen/quote_gen/ViewController.swift
@@ -7,12 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    // ui elements
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var quote: UILabel!
+    @IBOutlet weak var newquote: UIButton!
+    // quote array
+    let quotes = ["we have nothing to fear but fear itself", "I think, therefore I am", "Practice makes perfect", "Time is money"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func quotePressed(_ sender: UIButton)
+    {
+        let rand = Int.random(in: 0...(quotes.count - 1))
+        quote.text = quotes[rand]
+    }
+    
 
 
 }
